96c6d10b2025569fe91ea91c7e040391858a517c
Sharan <sharanraam.13@gmail>
2026-03-17 13:54:47 +0530

    UC3

--- OOPSBANNERAPPPLICATION.java
+++ OOPSBANNERAPPPLICATION.java
@@ -2,12 +2,9 @@ public class OOPSBANNERAPPPLICATION {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * ******  ***** ");
-        System.out.println("*     * *     * *             *");
-        System.out.println("*     * *     * *       *     *");
-        System.out.println(" *****   *****  *        ***** ");
+        String line1 = String.join(" ",
+        "*****", "*****", "*****", "*****");
+
+System.out.println(line1);
     }
 }
